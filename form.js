cca00ab156df57b70215a8069132f60f8f02a9e5
Naveed Akhtar <balochmna188@gmail.com>
2024-09-17 20:42:25 +0500

    portfolio

--- form.js
+++ form.js
@@ -3,7 +3,7 @@ const form = document.forms['contact-form']
 form.addEventListener('submit', e => {
 e.preventDefault()
 fetch(scriptURL, { method: 'POST', body: new FormData (form)})
-.then(response => alert("Thank you! your form is submitted successfully."))
+.then(response => alert("Thank you! your message is submitted successfully."))
 .then(() => { window.location.reload(); })
 .catch(error => console.error('Error!', error.message))
 })
